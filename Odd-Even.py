b607fc3b03615ddb5628d1a64429fe1290e356c3
Heherson Dela Cruz <delacruzheherson4477@gmail.com>
2023-04-27 17:25:30 +0800

    Segregate the file Integers.txt whether it is odd or even

--- Odd-Even.py
+++ Odd-Even.py
@@ -3,6 +3,15 @@ file = open("Integers.txt", "rt")
 for i in file:
     if i.strip:
         num = int(i)
+        
+        #Segregate the numbers in the "Integers.txt" 
+        # if it's even or odd then will create a file for even and odd numbers
+        if (num % 2 == 0):
+            even = open("even.txt", "a")
+            even.write(str(num))
+            even.write("\n")
+        else:
+            odd = open("odd.txt", "a")
+            odd.write(str(num))
+            odd.write("\n")
 
-        # print numbers
-        print(num)
